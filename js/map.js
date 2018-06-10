af64869ab10b8449e7aa3e83726409d79e4e7dbb
Winkii <plushka002@gmail.com>
2018-06-11 01:10:04 +0300

    Render card & pins

--- js/map.js
+++ js/map.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var cardTemplate = document.querySelector('template').content;
+var cardTemplate = document.querySelector('template');
+var mapCard = cardTemplate.content.querySelector('.map__card');
+var mapPin = cardTemplate.content.querySelector('.map__pin');
 var map = document.querySelector('.map');
 var mapFiltersContainer = document.querySelector('.map__filters-container');
 var nearestAdverts = [];
+var renderAds = [];
 
-var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира',
+var titleChoices = ['Большая уютная квартира', 'Маленькая неуютная квартира',
   'Огромный прекрасный дворец', 'Маленький ужасный дворец',
   'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
   'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
@@ -52,7 +55,7 @@ for (var i = 1; i <= 8; i++) {
       'avatar': 'img/avatars/user0' + i + '.png'
     },
     'offer': {
-      'title': getUniqueItem(TITLES),
+      'title': getUniqueItem(titleChoices),
       'price': getRandomInt(1000, 1000000),
       'type': TRANSLATE_TYPE[getRandomChoice(TYPES)],
       'rooms': getRandomInt(1, 5),
@@ -72,46 +75,48 @@ for (var i = 1; i <= 8; i++) {
   advert.offer.address = locationX + ', ' + locationY;
   nearestAdverts.push(advert);
 }
-
 map.classList.remove('map--faded');
 
-var getMapElement = function (element) {
-  var clonedElement = cardTemplate.cloneNode(true);
-  clonedElement.querySelector('.popup__title').textContent
+nearestAdverts.forEach(function (element) {
+  var clonedPin = mapPin.cloneNode(true);
+  clonedPin.querySelector('img').src = element.author.avatar;
+  clonedPin.style.left
+    = (element.location.x - pinHalfWidth) + 'px';
+  clonedPin.style.top
+    = (element.location.y - pinHeight) + 'px';
+  map.appendChild(clonedPin);
+});
+
+nearestAdverts.forEach(function (element) {
+  var clonedCard = mapCard.cloneNode(true);
+  clonedCard.querySelector('.popup__title').textContent
     = element.offer.title;
-  clonedElement.querySelector('.popup__text--address').textContent
+  clonedCard.querySelector('.popup__text--address').textContent
     = element.offer.address;
-  clonedElement.querySelector('.popup__text--price').textContent =
+  clonedCard.querySelector('.popup__text--price').textContent =
     element.offer.price + ' ₽/ночь';
-  clonedElement.querySelector('.popup__type').textContent = element.offer.type;
-  clonedElement.querySelector('.popup__text--capacity').textContent
+  clonedCard.querySelector('.popup__type').textContent = element.offer.type;
+  clonedCard.querySelector('.popup__text--capacity').textContent
     = element.offer.rooms + ' комнаты для ' + advert.offer.guests + ' гостей';
-  clonedElement.querySelector('.popup__text--time').textContent
+  clonedCard.querySelector('.popup__text--time').textContent
     = 'Заезд после ' + advert.offer.checkin + ', выезд до '
     + element.offer.checkout;
-  clonedElement.querySelector('.popup__features').textContent
+  clonedCard.querySelector('.popup__features li').textContent
     = element.offer.features;
-  clonedElement.querySelector('.popup__description').textContent
+  clonedCard.querySelector('.popup__description').textContent
     = element.offer.description;
-  clonedElement.querySelector('.popup__photo').src = element.offer.photos[0];
+  clonedCard.querySelector('.popup__photo').src = element.offer.photos[0];
 
   for (i = 1; i < element.offer.photos.length; i++) {
     var image = new Image(45, 40);
     image.src = element.offer.photos[i];
     image.classList.add('popup__photo');
     image.setAttribute('alt', 'Фотография жилья');
-    clonedElement.querySelector('.popup__photos').appendChild(image);
+    clonedCard.querySelector('.popup__photos').appendChild(image);
   }
-
-  clonedElement.querySelector('.popup__avatar').src = element.author.avatar;
-  clonedElement.querySelector('.map__pin img').src = element.author.avatar;
-  clonedElement.querySelector('.map__pin').style.left
-    = (element.location.x - pinHalfWidth) + 'px';
-  clonedElement.querySelector('.map__pin').style.top
-    = (element.location.y - pinHeight) + 'px';
-  return clonedElement;
-};
-
-nearestAdverts.forEach(function (element) {
-  map.insertBefore(getMapElement(element), mapFiltersContainer);
+  clonedCard.querySelector('.popup__avatar').src = element.author.avatar;
+  renderAds.push(clonedCard);
 });
+
+map.insertBefore(renderAds[0], mapFiltersContainer);
+
