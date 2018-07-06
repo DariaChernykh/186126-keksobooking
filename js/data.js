213b73112581c592e9ad31178ff1f3602c2a37fe
Winkii <plushka002@gmail.com>
2018-07-06 23:49:49 +0300

    Refactoring code.Add corrs 3

--- js/data.js
+++ js/data.js
@@ -14,37 +14,37 @@
       {
         isValid: checkString(advert.offer.title),
         data: advert.offer.title,
-        class: '.popup__title',
+        name: '.popup__title',
       },
       {
         isValid: checkString(advert.offer.address),
         data: advert.offer.address,
-        class: '.popup__text--address',
+        name: '.popup__text--address',
       },
       {
         isValid: checkNumber(advert.offer.price),
         data: advert.offer.price + ' ₽/ночь',
-        class: '.popup__text--price',
+        name: '.popup__text--price',
       },
       {
         isValid: checkString(advert.offer.type),
         data: TRANSLATE_TYPE[advert.offer.type],
-        class: '.popup__type',
+        name: '.popup__type',
       },
       {
         isValid: checkNumber(advert.offer.rooms) && checkNumber(advert.offer.guests),
         data: advert.offer.rooms + ' комнаты для ' + advert.offer.guests + ' гостей',
-        class: '.popup__text--capacity',
+        name: '.popup__text--capacity',
       },
       {
         isValid: checkString(advert.offer.checkin) && checkString(advert.offer.checkout),
         data: 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout,
-        class: '.popup__text--time',
+        name: '.popup__text--time',
       },
       {
         isValid: checkString(advert.offer.description),
         data: advert.offer.description,
-        class: '.popup__description',
+        name: '.popup__description',
       }
     ];
 
